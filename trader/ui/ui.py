81454c9c6d29b8c148a25f6671288b36081224dd
Eugene Feshchenko <efeshchenko@bedroomfurniturediscounts.com>
2018-02-12 13:18:24 +0200

    ui enhancement

--- trader/ui/ui.py
+++ trader/ui/ui.py
@@ -9,23 +9,33 @@ app = Flask(__name__)
 
 @app.template_filter('currency')
 def format_currency(value):
+    value = float(value)
     return "{:,.2f}".format(value)
+
+@app.template_filter('shortdate')
+def shortdate(value):
+    try:
+        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%SZ')
+        value = value.strftime('%y-%m-%d %H:%M')
+    except:
+        pass
+    return value
 
 
 @app.route('/')
 def hello_world():
     client = KunaApiClient(API_KEY, API_SECRET)
     eth_tick = client.get_tick()
-
-    for key in eth_tick['ticker'].keys():
-        eth_tick['ticker'][key] = float(eth_tick['ticker'][key])
-
     actives = client.get_balance()
     at = datetime.fromtimestamp(eth_tick['at'])
     orders = client.get_active_orders()
+    deals = client.get_trades_history()
+    deals = [x for x in deals if x['market']=='ethuah'][:10]
+
     with open('../../logs/trader.log', 'r') as f:
         logs = f.read()
-    logs = '\n'.join(logs.splitlines()[-5:])
+
+    logs = logs.splitlines()[-10:]
 
     return render_template('index.html',
                            at=at,
@@ -33,4 +43,5 @@ def hello_world():
                            actives=actives,
                            orders=orders,
                            logs=logs,
+                           deals=deals,
                            )
