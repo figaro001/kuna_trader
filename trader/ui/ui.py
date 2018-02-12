f5f1bede6d2d9e52d7c2a7942035d4a1fd098471
Eugene Feshchenko <efeshchenko@bedroomfurniturediscounts.com>
2018-02-12 11:49:10 +0200

    ui improvement

--- trader/ui/ui.py
+++ trader/ui/ui.py
@@ -7,10 +7,19 @@ from ..credentials import API_KEY, API_SECRET
 
 app = Flask(__name__)
 
+@app.template_filter('currency')
+def format_currency(value):
+    return "{:,.2f}".format(value)
+
+
 @app.route('/')
 def hello_world():
     client = KunaApiClient(API_KEY, API_SECRET)
     eth_tick = client.get_tick()
+
+    for key in eth_tick['ticker'].keys():
+        eth_tick['ticker'][key] = float(eth_tick['ticker'][key])
+
     actives = client.get_balance()
     at = datetime.fromtimestamp(eth_tick['at'])
     orders = client.get_active_orders()
