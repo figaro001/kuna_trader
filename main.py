55ef015f93286a34c6fc833f9604ffd602e43ad7
Eugene Feshchenko <efeshchenko@bedroomfurniturediscounts.com>
2018-03-07 10:59:35 +0200

    history save format update WIP

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 from datetime import datetime
 from celery import Celery, chain
 from celery.schedules import crontab
@@ -10,7 +11,8 @@ import pandas as pd
 from kuna_api import KunaApiClient
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_FILE_PATH = os.path.join(BASE_DIR, 'data', 'historical.csv')
+CSV_DATA_FILE_PATH = os.path.join(BASE_DIR, 'data', 'historical.csv')
+DB_DATA_FILE_PATH = os.path.join(BASE_DIR, 'data', 'historical.db')
 DATA_URL = 'http://192.168.0.105:5000/data'
 #DATA_URL = 'http://localhost:5000/data'
 LOG_FILE_PATH = os.path.join(BASE_DIR, 'logs', 'celery_supervisor_err.log')
@@ -57,7 +59,7 @@ def main():
         logs = f.read()
     logs = logs.splitlines()[-5:]
 
-    data = pd.read_csv(DATA_FILE_PATH, index_col=0)
+    data = pd.read_csv(CSV_DATA_FILE_PATH, index_col=0)
     data = data.set_index('timestamp')
 
     data['short_mavg'] = data['sell'].rolling(window=93, min_periods=1, center=False).mean()
@@ -82,7 +84,7 @@ def main():
 
 @app.route('/data')
 def historical():
-    data = pd.read_csv(DATA_FILE_PATH, index_col=0)
+    data = pd.read_csv(CSV_DATA_FILE_PATH, index_col=0)
     data = data.set_index('timestamp')
     data = data.to_json(orient='index')
     return Response(response=data, status=200, mimetype='application/json')
@@ -95,22 +97,25 @@ def process_signals(_):
 
 @celery.task
 def save_history():
-    if os.path.exists(DATA_FILE_PATH):
-        historical_data = pd.read_csv(DATA_FILE_PATH, index_col=0)
-    else:
+    if not os.path.exists(CSV_DATA_FILE_PATH):
         columns = ['timestamp', 'buy', 'sell', 'low', 'high', 'last', 'vol']
         historical_data = pd.DataFrame(columns=columns)
+        historical_data = historical_data.set_index('timestamp')
+        historical_data.to_csv(CSV_DATA_FILE_PATH)
 
     data = KunaApiClient().get_tick()
-    df = pd.DataFrame({'timestamp': data['at'],
-                       'buy': data['ticker']['buy'],
-                       'sell': data['ticker']['sell'],
-                       'low': data['ticker']['low'],
-                       'high': data['ticker']['high'],
-                       'last': data['ticker']['last'],
-                       'vol': data['ticker']['vol']}, index=[0])
-    historical_data = pd.concat([historical_data, df])
-    historical_data.to_csv(DATA_FILE_PATH)
+    df = pd.DataFrame({'timestamp': datetime.fromtimestamp(int(data['at'])),
+                       'buy': float(data['ticker']['buy']),
+                       'sell': float(data['ticker']['sell']),
+                       'low': float(data['ticker']['low']),
+                       'high': float(data['ticker']['high']),
+                       'last': float(data['ticker']['last']),
+                       'vol': float(data['ticker']['vol'])}, index=[0])
+    df = df.set_index('timestamp')
+    df.to_csv(CSV_DATA_FILE_PATH, mode='a', header=False)
+
+    conn = sqlite3.connect(DB_DATA_FILE_PATH)
+    df.to_sql(name='signals', con=conn, if_exists='append')
 
 
 @celery.task()
